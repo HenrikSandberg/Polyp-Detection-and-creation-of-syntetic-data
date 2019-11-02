f81435b4ad7515912ae147f4a38a90492d9096c3
Henrik sandberg <henrik.a.sandberg@icloud.com>
2019-11-02 20:57:18 +0100

    Update model.py

--- model.py
+++ model.py
@@ -17,9 +17,6 @@ from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPool2D, Flatten, Dense
 
-#Fitting the CNN to images
-from keras.preprocessing.image import ImageDataGenerator
-
 #Making new prediction
 import numpy as np
 from keras.preprocessing import image
@@ -63,38 +60,18 @@ def create_training_data():
 
 def build_model():
     model = Sequential([
-        Conv2D(64, (3, 3), activation='relu', input_shape=(img_size, img_size, 3)),
+        Conv2D(256, (3, 3), activation='relu', input_shape=(img_size, img_size, 3)),
         MaxPool2D((2, 2)),
         Conv2D(128, (3, 3), activation='relu'),
         MaxPool2D((2, 2)),
-        Conv2D(128, (3, 3), activation='relu'),
         Flatten(),
-        Dense(128, activation='relu'),
-        Dense(8, activation='softmax')
+        Dense(units=128, activation='relu'),
+        Dense(units=8, activation="sigmoid")
     ])
-
-    base_model = keras.applications.ResNet50(
-        include_top=False,
-        weights="imagenet",
-        input_shape=(img_size, img_size, 3)
-    )
-
-    golbal_avg_pool = keras.layers.GlobalAveragePooling2D()(base_model.output)
-    output = Dense(8, activation="softmax")(golbal_avg_pool)
-    model = Model(inputs=base_model.input, outputs=output)
-    model.summary()
     return model
 
 X = []
 y = []
-
-''' try:
-    pickle_in = open("X.pickle","rb")
-    X = pickle.load(pickle_in)
-    pickle_in = open("y.pickle","rb")
-    y = pickle.load(pickle_in)
-    # X = X/255.0
-except Exception as e: '''
 training_data = create_training_data()
 
 for feature, label in training_data:
@@ -102,37 +79,12 @@ for feature, label in training_data:
     y.append(label)
 
 X = np.array(X).reshape(-1, img_size, img_size, 3)
-X = X/255
-
-''' pickle_out = open('X.pickle', 'wb')
-pickle.dump(X, pickle_out)
-pickle_out.close()
-
-pickle_out = open('y.pickle', 'wb')
-pickle.dump(y, pickle_out)
-pickle_out.close() '''
 
 model = build_model()
 
 model.compile(
-    optimizer='adam',
     loss='sparse_categorical_crossentropy',
-    metrics=[ 'accuracy' ]
-)
+    optimizer='adam',
+    metrics=['accuracy'])
 
-#Add a callback so that we can use tensorboard
-logdir = os.path.join("logs_l8", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
-
-history = model.fit(X,y,
-    batch_size=32,
-    epochs=10, 
-    validation_split=0.15,
-)
-
-''' callbacks=[
-    tensorboard_callback,
-    keras.callbacks.Model.Checkpoint('model.h5', monitor='val_loss')
-]
-model = keras.models.load_data('model.h5')
- '''
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.15) 
