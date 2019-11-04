2441caf9db8d9e2583a27293e58e19ac797aae63
Henrik sandberg <henrik.a.sandberg@icloud.com>
2019-11-04 12:06:27 +0100

    Moved code into src folder

--- src/model.py
+++ src/model.py
@@ -15,7 +15,7 @@ from tensorflow import keras
 
 #Import from keras
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPool2D, Flatten, Dense
+from keras.layers import Conv2D, GlobalAvgPool2D, MaxPool2D, Flatten, Dense
 
 #Making new prediction
 import numpy as np
@@ -25,8 +25,14 @@ from keras.preprocessing import image
 import pickle
 
 #GLOBALE VALUES
-img_size = 256
+img_size = 500
+img_width = 576
+img_height = 720
+
 data_dir = 'data/'
+
+X = []
+y = []
 
 CATEGORIES = [
     'dyed-lifted-polyps', 
@@ -47,7 +53,7 @@ def create_training_data():
         class_num = CATEGORIES.index(category)
         for img in os.listdir(path):
             try:
-                img_array = cv2.imread(os.path.join(path,img)) #TODO: May want to make img gray -> can add: , cv2.IMREAD_GRAYSCALE
+                img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE) #TODO: May want to make img gray -> can add: 
                 new_img_array = cv2.resize(img_array, (img_size, img_size))
                 training_data.append([new_img_array, class_num])
                 # plt.imshow(img_array, cmap='gray')
@@ -60,7 +66,9 @@ def create_training_data():
 
 def build_model():
     model = Sequential([
-        Conv2D(256, (3, 3), activation='relu', input_shape=(img_size, img_size, 3)),
+        Conv2D(256, (3, 3), activation='relu', input_shape=(img_size, img_size, 1)),
+        MaxPool2D((2, 2)),
+        Conv2D(128, (3, 3), activation='relu'),
         MaxPool2D((2, 2)),
         Conv2D(128, (3, 3), activation='relu'),
         MaxPool2D((2, 2)),
@@ -70,15 +78,13 @@ def build_model():
     ])
     return model
 
-X = []
-y = []
 training_data = create_training_data()
 
 for feature, label in training_data:
     X.append(feature)
     y.append(label)
 
-X = np.array(X).reshape(-1, img_size, img_size, 3)
+X = np.array(X).reshape(-1, img_size, img_size, 1)
 
 model = build_model()
 
@@ -87,4 +93,15 @@ model.compile(
     optimizer='adam',
     metrics=['accuracy'])
 
-model.fit(X, y, batch_size=32, epochs=10, validation_split=0.15) 
+history = model.fit(X, y, 
+    batch_size=32, 
+    epochs=10, 
+    validation_split=0.05) 
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+plt.show()
